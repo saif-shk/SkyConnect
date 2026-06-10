c08a910785bb160e463347ef1b91f604bc072438
saif-shk <saifshaikh7806@gmail.com>
2026-06-10 22:42:31 +0530

    fix: resolve hardcoded api server url in production environment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,5 +1,5 @@
 const server = process.env.NODE_ENV === 'production' ?
-    "https://skyconnect-backend2.onrender.com" :
+    (process.env.REACT_APP_BACKEND_URL || "https://skyconnect-backend-n0x2.onrender.com") :
     "http://localhost:8000"
 
 
